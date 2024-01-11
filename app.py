60a6021f64a63dcb3aac670283a218d94836af00
Anas Maroof <139492221+anas-maroof@users.noreply.github.com>
2024-01-12 01:25:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
 import yfinance as yf
 
